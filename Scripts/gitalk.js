e02eb3707c58799334c9f17218b961f48df0dba9
pq <alan.funs@gmail.com>
2022-03-10 15:45:49 +0800

    Publish deploy 2022-03-10 15:45

---
+++ Scripts/gitalk.js
@@ -0,0 +1,5 @@
+(function() {
+    var t = document,
+        e = t.createElement("script");
+    e.src = "https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.min.js", e.setAttribute("data-timestamp", +new Date), (t.head || t.body).appendChild(e)
+})();
