134117c085ac4b41d0d4f089f39dd751b5d74389
pdso <alan.funs@gmail.com>
2022-03-10 21:49:24 +0800

    Publish deploy 2022-03-10 21:49

--- Scripts/gitalk.js
+++
@@ -1,5 +0,0 @@
-(function() {
-    var t = document,
-        e = t.createElement("script");
-    e.src = "https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.min.js", e.setAttribute("data-timestamp", +new Date), (t.head || t.body).appendChild(e)
-})();
